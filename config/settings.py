6c02af1c7d14f0fd5446f3cbde0b5578012d9bb8
Aniket <admin@polynexus.in>
2026-06-12 22:22:04 +0530

    Define STATICFILES_DIRS to include root static folder

--- config/settings.py
+++ config/settings.py
@@ -73,8 +73,12 @@ TIME_ZONE = "UTC"
 USE_TZ = True
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STORAGES = {"staticfiles": {"BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage"}}
+
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_URL = "login"
